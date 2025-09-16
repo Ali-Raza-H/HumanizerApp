fdebe2bf112c7af7a131a6962abce868acca7eee
Ali-Raza-H <dannygamer.ali@gmail.com>
2025-09-16 23:57:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ nltk.download("wordnet")
 nltk.download("omw-1.4")
 
 app = Flask(__name__)
-CORS(app, resources={r"/humanize": {origins: "*"}})  # <-- allow JS to call backend
+CORS(app, resources={r"/humanize": {"origins": "*"}})  # <-- allow JS to call backend
 
 def get_synonym(word):
     synonyms = []
@@ -49,4 +49,5 @@ def humanize_api():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
