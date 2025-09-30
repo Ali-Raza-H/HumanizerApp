674c6f41e57e57d710534ff3d9dcc8c7efd3bace
Ali-Raza-H <dannygamer.ali@gmail.com>
2025-09-30 20:16:39 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Only install Python dependencies
+# Install ncurses for tput (if using colors in scripts)
+apt-get update
+apt-get install -y ncurses-bin
+
+# Install Java (needed for language_tool_python)
+apt-get install -y openjdk-17-jre-headless
+
+# Install Python dependencies
 pip install -r requirements.txt
