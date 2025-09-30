e3058853673d70903d3f7a97b50253f20ea56c08
Ali-Raza-H <dannygamer.ali@gmail.com>
2025-09-30 19:52:36 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Install Java (needed for language_tool_python)
-apt-get update
-apt-get install -y openjdk-17-jre-headless
-
-# Install Python dependencies
+# Only install Python dependencies
 pip install -r requirements.txt
